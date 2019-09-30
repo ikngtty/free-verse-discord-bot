d32cb30562886ba019f1dd4741755796c3e3c769
ikngtty <ikngtty@gmail.com>
2019-09-30 21:57:39 +0900

    :sparkles: the rule changes daily

--- lib/rule_generator.rb
+++ lib/rule_generator.rb
@@ -3,12 +3,24 @@
 class RuleGenerator
   attr_accessor :ranges
 
-  def initialize(get_rand)
+  def initialize(get_rand, get_today)
     @ranges = [1..10] * 3
     @get_rand = get_rand
+    @get_today = get_today
+    @last_rule = nil
+    @last_called_date = nil
   end
 
   def call
-    ranges.map { |range| @get_rand.call(range) }
+    today = @get_today.call
+    return @last_rule if today == @last_called_date
+
+    # include 2 patterns:
+    # * first call (@last_called_date is nil)
+    # * first call in a different day (@last_called_date is not nil)
+    new_rule = ranges.map { |range| @get_rand.call(range) }
+    @last_rule = new_rule
+    @last_called_date = today
+    new_rule
   end
 end
--- spec/rule_generator_spec.rb
+++ spec/rule_generator_spec.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+require 'date'
+
 require 'rspec'
+
 require_relative '../lib/rule_generator'
 
 class GetRandomStub
@@ -18,18 +21,36 @@ class GetRandomStub
 end
 
 RSpec.describe RuleGenerator do
-  subject(:generator) { described_class.new(get_random) }
+  subject(:generator) { described_class.new(get_random, get_today) }
 
   let(:get_random) { GetRandomStub.new }
+  let(:get_today) { object_double(Date.method(:new), call: today) }
+  let(:today) { Date.new(1982, 12, 6) }
 
   describe '#call' do
     it 'generates a rule which uses get_random for each length' do
       expect(generator.call).to eq [1, 2, 3]
     end
 
-    it 'changes the result by every call' do
+    it 'does not change the rule at the same day' do
+      first = generator.call
+      second = generator.call
+      expect(second).to eq first
+    end
+
+    it 'changes the rule at the different day' do
+      first = generator.call
+      allow(get_today).to receive_messages(call: today + 1)
+      second = generator.call
+      expect(second).not_to eq first
+    end
+
+    it 'does not change the rule at the same day after the change' do
       generator.call
-      expect(generator.call).to eq [4, 5, 6]
+      allow(get_today).to receive_messages(call: today + 1)
+      second = generator.call
+      third = generator.call
+      expect(third).to eq second
     end
 
     it 'generates a rule which each length cannot be over 10' do
