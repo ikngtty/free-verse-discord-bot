b981a1ae25fd1ff0d1f44b59883ecbfe3947bda3
ikngtty <ikngtty@gmail.com>
2020-07-18 13:03:00 +0900

    :green_heart: split `VerseRuleHolder` class in two 1/?
    Add `VerseRuleRepositoryMemory` class.

--- lib/verse_rule_holder.rb
+++ lib/verse_rule_holder.rb
@@ -1,21 +1,23 @@
 # frozen_string_literal: true
 
+require_relative 'verse_rule_repository_memory'
+
 class VerseRuleHolder
   def initialize(generate_rule, get_today)
     @generate_rule = generate_rule
     @get_today = get_today
-    @last_rule = nil
+    @repo = VerseRuleRepositoryMemory.new
   end
 
   def current
     today = @get_today.call
-    return @last_rule if today == @last_rule&.created_at
+    return @repo.current if today == @repo.current&.created_at
 
     # include 2 patterns:
-    # * first call (@last_rule is nil)
-    # * first call in a different day (@last_rule is not nil)
-    @last_rule = @generate_rule.call
-    puts "New Rule! #{@last_rule.values}"
-    @last_rule
+    # * first call (@repo.current is nil)
+    # * first call in a different day (@repo.current is not nil)
+    @repo.current = @generate_rule.call
+    puts "New Rule! #{@repo.current.values}"
+    @repo.current
   end
 end
