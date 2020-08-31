ff4187d4e2c94bce13021f237fe73e3012365fad
ikngtty <ikngtty@gmail.com>
2020-09-01 02:14:13 +0900

    :green_heart: make `analyze` function for SoC

--- lib/discord_event_handler.rb
+++ lib/discord_event_handler.rb
@@ -17,25 +17,40 @@ class DiscordEventHandler
     respond = event.method(:respond)
 
     managed_role = member.roles.find(&:managed?)
-    match = command_regexp(bot_id, managed_role.id).match(event.content)
-    if match
-      captures = match.named_captures
-      command = captures['command']
-      args_text = captures['args']
-      case command
+    message_text = event.content
+    result = analyze(message_text, bot_id, managed_role.id)
+    if result.command?
+      case result.command
       when 'mecab'
-        @bot.mecab_command(args_text, respond)
+        @bot.mecab_command(result.args_text, respond)
       else
         @bot.unknown_command(respond)
       end
     else
-      @bot.detect(event.content, respond)
+      @bot.detect(message_text, respond)
     end
   end
 
+  AnalyzeResult = Struct.new(
+    :command?,
+    :command,
+    :args_text,
+    keyword_init: true
+  )
+  private_constant :AnalyzeResult
+
   private
 
-  def command_regexp(bot_id, managed_role_id)
-    /\A (?: \< @#{bot_id} \> | \< @!#{bot_id} \> | \< @&#{managed_role_id} \>) \s* (?<command> \S+) \s* (?<args> .*) \Z/mx
+  def analyze(message_text, bot_id, managed_role_id)
+    re = /\A (?: \< @#{bot_id} \> | \< @!#{bot_id} \> | \< @&#{managed_role_id} \>) \s* (?<command> \S+) \s* (?<args> .*) \Z/mx
+    match = re.match(message_text)
+    return AnalyzeResult.new(command?: false) if match.nil?
+
+    captures = match.named_captures
+    AnalyzeResult.new(
+      command?: true,
+      command: captures['command'],
+      args_text: captures['args']
+    )
   end
 end
